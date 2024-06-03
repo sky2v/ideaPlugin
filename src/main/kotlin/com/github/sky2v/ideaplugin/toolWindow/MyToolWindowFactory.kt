3d3650c05a78a540a6cd19238c93f47119dbb385
GitHub Action <action@github.com>
2024-06-03 10:46:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/sky2v/ideaplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/sky2v/ideaplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.sky2v.ideaplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sky2v.ideaplugin.MyBundle
+import com.github.sky2v.ideaplugin.services.MyProjectService
 import javax.swing.JButton
 
 
